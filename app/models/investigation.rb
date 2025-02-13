577dd6a9cf90c3db1ccf7483a7218d80435ff1cd
Tom Dooner <tomdooner@gmail.com>
2025-02-12 19:32:13 -0800

    Simplify rendering by moving it to partials

--- app/models/investigation.rb
+++ app/models/investigation.rb
@@ -7,43 +7,6 @@ class Investigation < ApplicationRecord
   end
 
   def to_html
-    # TODO: This is whack. It should be a partial.
-    location = [ contents_raw["cm_city"], contents_raw["cm_state"], contents_raw["cm_country"] ].compact.join(", ")
-    date = DateTime.parse(contents_raw["cm_eventDate"]).strftime("%m/%d/%Y")
-    first_vehicle = Array(contents_raw["cm_vehicles"]).first
-    mishap =
-      if first_vehicle
-        first_vehicle["cm_events"].first["cicttEventSOEGroup"]
-      else
-        "Unknown Mishap"
-      end
-
-    injuries = Array(first_vehicle["cm_injuries"]).first
-    injuries_text =
-      if injuries
-        "#{injuries["cm_crew_Fatal"]} Fatal, #{injuries["cm_crew_Serious"]} Serious, #{injuries["cm_crew_None"]} None"
-      else
-        "Unknown Injuries"
-      end
-    report_url = "https://data.ntsb.gov/carol-repgen/api/Aviation/ReportMain/GenerateNewestReport/#{contents_raw["cm_mkey"]}/pdf"
-
-    base_details = <<~HTML
-      <li><strong>Location:</strong> #{location}</li>
-      <li><strong>Date:</strong> #{date}</li>
-      <li><strong>Details:</strong> #{mishap} / #{injuries_text}</li>
-    HTML
-    report_details = <<~HTML if most_recent_report_type.present?
-      <li>
-        <strong>Report URL:</strong> <a href="#{report_url}" target="_blank" rel="noopener noreferer">Download Latest Report</a> (Dated #{contents_raw["cm_recentReportPublishDate"]})
-      </li>
-    HTML
-
-    <<~HTML.html_safe
-      <ul>
-        #{base_details}
-        #{report_details}
-      </ul>
-    HTML
   end
 
   def assign_ntsb_json_attributes(json)
