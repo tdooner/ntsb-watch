e84c705867d9a53a55f860b2a748d6d1592b78a0
Tom Dooner <tomdooner@gmail.com>
2025-06-19 14:22:56 -0700

    Use 24 hour lookback for email instead of previous calendar day

--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -1,5 +1,5 @@
 class DailyMailer < ApplicationMailer
-  def daily_mail(email, interval: Date.yesterday.all_day)
+  def daily_mail(email, interval: 24.hours.ago..Time.now)
     @differences = DailySyncDifference.where(created_at: interval).includes(:investigation)
 
     mail to: email, subject: "NTSB Daily Alert for #{Date.today}"
