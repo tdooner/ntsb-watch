fd7549cc9fb72731fdc4223e56f2dd5bba36dc5f
Tom Dooner <tomdooner@gmail.com>
2025-06-19 12:54:22 -0700

    Add activeadmin, devise, and activeadmin_assets gems

--- Gemfile
+++ Gemfile
@@ -41,11 +41,6 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
-# Other application dependencies (keep in alphabetical order):
-gem "dotenv-rails"
-gem "mailgun-ruby"
-gem "rubyzip"
-
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
@@ -69,3 +64,11 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+# Other application dependencies (keep in alphabetical order):
+gem "activeadmin", "= 4.0.0.beta15"
+gem "activeadmin_assets", "~> 1.0"
+gem "devise", "~> 4.9"
+gem "dotenv-rails"
+gem "mailgun-ruby"
+gem "rubyzip"
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_10_013250) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_19_195338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
+  end
 
   create_table "daily_sync_differences", force: :cascade do |t|
     t.bigint "investigation_id", null: false
@@ -32,6 +46,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_10_013250) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ntsb_mkey"], name: "index_investigations_on_ntsb_mkey", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "daily_sync_differences", "investigations"
