581a7f7e2d5bdae17cf64d6e40487799062ad444
Tom Dooner <tomdooner@gmail.com>
2025-06-19 13:44:20 -0700

    Fix rubocop lints with `rubocop -A`

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.mailgun_settings = {
     api_key: ENV["MAILGUN_API_KEY"],
-    domain: "tdooner.com",
+    domain: "tdooner.com"
     # timeout: 20 # Default depends on rest-client, whose default is 60s. Added in 1.2.3.
   }
 end
