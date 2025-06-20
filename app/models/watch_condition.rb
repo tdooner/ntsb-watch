25c16da44f8bfddc1c421b567cb4bd2b93c49bf4
Tom Dooner <tomdooner@gmail.com>
2025-06-19 22:56:39 -0700

    Add ability to watch a specific airport

--- app/models/watch_condition.rb
+++ app/models/watch_condition.rb
@@ -2,6 +2,7 @@ class WatchCondition < ApplicationRecord
   VALID_KEYS = %i[
     registrationNumber
     prelimNarrative
+    airportId
   ]
 
   belongs_to :user
@@ -13,6 +14,8 @@ class WatchCondition < ApplicationRecord
       Investigation.where("jsonb_path_exists(contents_raw, ('$.cm_vehicles[*].registrationNumber ? (@ == \"' || :value || '\")')::jsonpath)", value: condition_value)
     when "prelimNarrative"
       Investigation.where("contents_raw->>'prelimNarrative' ilike ?", "%#{condition_value}%")
+    when "airportId"
+      Investigation.where("contents_raw->>'airportId' = ? OR contents_raw->>'airportId' = ?", condition_value, "K#{condition_value}")
     end
   end
 end
--- spec/models/watch_condition_spec.rb
+++ spec/models/watch_condition_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe WatchCondition, type: :model do
   describe "#matching_investigations" do
     let!(:investigation) { create(:investigation, fixture_name: "N666DS.json") }
+    let!(:investigation_2) { create(:investigation, fixture_name: "N653ND.json") }
 
     subject { watch_condition.matching_investigations }
 
@@ -31,6 +32,26 @@ RSpec.describe WatchCondition, type: :model do
         let(:watch_condition) { create(:watch_condition, condition_key: "prelimNarrative", condition_value: "Part 121") }
 
         it { is_expected.not_to include(investigation) }
+      end
+    end
+
+    context "for condition_key = airportId" do
+      context "for a matching condition_value" do
+        let(:watch_condition) { create(:watch_condition, condition_key: "airportId", condition_value: "KSQL") }
+
+        it { is_expected.to include(investigation_2) }
+      end
+
+      context "for a matching condition_value (without K prefix)" do
+        let(:watch_condition) { create(:watch_condition, condition_key: "airportId", condition_value: "SQL") }
+
+        it { is_expected.to include(investigation_2) }
+      end
+
+      context "for a non-matching condition_value" do
+        let(:watch_condition) { create(:watch_condition, condition_key: "airportId", condition_value: "SFO") }
+
+        it { is_expected.not_to include(investigation_2) }
       end
     end
   end
